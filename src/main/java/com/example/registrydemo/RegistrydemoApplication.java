867dd46dc8eed016f29331ddee3895716ba257be
Fan Liu <charles_liuf@hotmail.com>
2018-11-20 10:56:01 +0800

    update for triggering pipeline

--- src/main/java/com/example/registrydemo/RegistrydemoApplication.java
+++ src/main/java/com/example/registrydemo/RegistrydemoApplication.java
@@ -58,7 +58,7 @@ public class RegistrydemoApplication {
 
 	@RequestMapping("/demo")
 	public String update() {
-		return "Hello concourse!!!-V0.3";
+		return "Hello concourse!!!-V0.4";
 	}
 
 	public static void main(String[] args) {
