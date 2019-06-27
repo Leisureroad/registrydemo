df4dea4d8e0f2ebce0a2cd6728d846429d7fc0be
leisureroad <charles_liuf@hotmail.com>
2019-06-27 12:31:54 +0800

    trigger pipeline

--- src/main/java/com/example/registrydemo/RegistrydemoApplication.java
+++ src/main/java/com/example/registrydemo/RegistrydemoApplication.java
@@ -58,7 +58,7 @@ public class RegistrydemoApplication {
 
 	@RequestMapping("/demo")
 	public String update() {
-		return "Hello concourse!!!-V1.0";
+		return "Hello concourse!!!-V1.1";
 	}
 
 	public static void main(String[] args) {
